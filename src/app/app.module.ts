718ad4658a4a7609b8fe3b6309f5f21c26f37cb0
reggmack <thegraphixninja@gmail.com>
2017-03-13 15:01:19 -0500

    Finalize
    Finalize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { ProfileComponent } from './components/profile.component';
 
 @NgModule({
   declarations: [
